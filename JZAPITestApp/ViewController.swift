c8cc0107ee3e0ae9c96c3d2dad5dc68b986c00a3
Yuliia Zhelem <jzhelem@appcodin.com>
2022-10-09 21:34:46 +0300

    Initial Commit

---
+++ JZAPITestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JZAPITestApp
+//
+//  Created by Iuliia Zhelem on 10/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
